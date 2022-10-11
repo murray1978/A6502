eacbe02c16d5758e693713c335c5ac50dd6486fa
Murray <muzzdeni78@gmail.com>
2022-10-11 21:39:39 +1300

    Update calc.s

--- Src/calc.s
+++ Src/calc.s
@@ -1,2 +1,22 @@
 ;Reverse polish calculator for A6502 board
-;Address 0x0000 - 0x0100 Zero page, 
+;Address  0x0000 - 0x00FF Zero page, Monitor/Bios
+;         0x0100 - 0x01FF  Stack
+;         0x0200 - 0x02FF  Key buffer (plus space)
+;         0x0400 - 0x05FF  FDC buffer (512 bytes)
+
+;Program Header
+PROG_BASE   EQU   $0600
+INP_BUFFER  EQU   $0200           ;Use the monitor buffer
+CURR_CHAR   EQU   PROG_BASE+$01   ;Position of current char
+
+RP_STACK_TOP   EQU  PROG_END+$0100
+RP_STACK_PTR   EQU
+
+ORG $0700
+  LDA #$00
+  STA CURR_CHAR   ;Set current character location to zero
+  
+  
+ MSG1 DB  "RP calculator, 10 10 +  > 100"
+ 
+ PROG_END:
